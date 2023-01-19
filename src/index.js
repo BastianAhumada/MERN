e96ec7a2a2a060124874e8b7e0025acc8f24eeaa
BastianAhumada <bastianahumadajimenez@gmail.com>
2023-01-18 21:19:38 -0300

    schema configuration server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import morgan from 'morgan'
+
+import Router from './routes/index.js'
 
 const app = express() // Servidor
 
-app.listen(3000, () => {
+//Settings
+app.set('port', process.env.PORT || 3001) // NameValue, Value
+
+//Midlewares -> Logics Befores Routes.
+app.use(morgan('dev')) // Logs Request Server Morgan Dependencies.
+app.use(express.json()) // Understand Request JSON.
+
+// Router
+app.use(Router) // Read File Routes
+
+app.listen(app.get('port'), () => { // Retrieve variable of port 
   console.log('Servidor APP')
 })
