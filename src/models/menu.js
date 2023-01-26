8c70b76abb9fa408c9ee6108c2a94b7cee41a6b9
BastianAhumada <bastianahumadajimenez@gmail.com>
2023-01-26 18:42:16 -0300

    Feat: Integrate WebPack and Babel

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,9 +1,11 @@
-import mongoose from 'mongoose'
-import { Schema } from mongoose //Import Schema Method
+import mongoose from 'mongoose';
+const { Schema } = mongoose; //Import Schema Method
 
-new Schema({
-  nombre: { type: String, required: true },  // Property, { type:, required: empty or not }
+const MenuSchema = new Schema({
+  name: { type: String, required: true },  // Property, { type:, required: empty or not }
   price: { type: Number, required: true},
   category: {type: String, required: true},
   // ingredients: {type: Arra}
 })
+
+export default mongoose.model('MenuSchema', MenuSchema) // Create Model MongoDB
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,25 @@
 import express from 'express'
+import MenuSchema from '../models/menu.js'
 
-const router = express.Router()
 
-router.get('/', (req,res) => { // One URL
-  res.json({
-    name: "work"
-  })
+const router = express.Router() // Ineheretence from Router.
+
+router.get('/', async (req, res) => { // One URL
+
+  const GetMenus = await MenuSchema.find()
 })
 
-router.get('/testUrl', (req,res) => { // Other URL 
-  res.send('Hello URLs')
+router.post('/insertDoc', async (req, res) => {
+
+  const { name, price, category } = req.body
+
+  const NewMenu = await new MenuSchema({    // Format Document
+    name,
+    price,
+    category
+  }).save()  // Save in DB 
+
+  res.json('Success')
 })
 
 export default router
